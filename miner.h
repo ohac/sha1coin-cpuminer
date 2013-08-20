8e8f9f0e12558f8921f411e864522737ea30ffc9
Neisklar <Neisklar@users.noreply.github.com>
2013-08-20 20:42:42 +0200

    fixing missing forward declaration
    (i still don't now why it worked locally)

--- miner.h
+++ miner.h
@@ -154,6 +154,7 @@ extern int scanhash_sha256d(int thr_id, uint32_t *pdata,
 extern int scanhash_quark(int thr_id, uint32_t *pdata,
 	const uint32_t *ptarget, uint32_t max_nonce, unsigned long *hashes_done);
 	
+extern void init_quarkhash_contexts();	
 	
 extern unsigned char *scrypt_buffer_alloc();
 extern int scanhash_scrypt(int thr_id, uint32_t *pdata,
