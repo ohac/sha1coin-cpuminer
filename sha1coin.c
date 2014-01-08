0dcdd9a5cb3983d3b0541cebd2cd0713d2f7581e
OHASHI Hideya <ohachige@gmail.com>
2014-01-08 19:48:26 +0900

    trip speed

--- sha1coin.c
+++ sha1coin.c
@@ -33,6 +33,22 @@ inline void encodeb64(const unsigned char* pch, char* buff)
 //*(buff + 1) = 0;
 }
 
+uint32_t decodeb64chunk(const char* str)
+{
+  unsigned int dec = 0;
+  const char *pbase64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+  for (int i = 0; i < 4; i++)
+  {
+    char* pos = strchr(pbase64, str[i]);
+    int v = (int)(pos - pbase64);
+    dec = (dec << 6) | v;
+  }
+  dec <<= 8;
+  uint32_t result = 0;
+  be32enc(&result, dec); 
+  return result;
+}
+
 inline void encodeb64chunk(const unsigned char* pch, char* buff)
 {
   const char *pbase64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
@@ -61,6 +77,8 @@ inline void encodeb64chunk(const unsigned char* pch, char* buff)
 
 static unsigned short b64tbl1[0x10000];
 static unsigned short b64tbl2[0x10000];
+const char *searchtrip = "sha1";
+uint32_t searchchunk = 0;
 
 void genb64tbl()
 {
@@ -78,6 +96,7 @@ void genb64tbl()
     encodeb64chunk(in, out);
     b64tbl2[i] = out[2] | (out[3] << 8);
   }
+  searchchunk = decodeb64chunk(searchtrip);
 }
 
 void encodeb64wide(const unsigned char* pch, unsigned short* buff)
@@ -123,9 +142,9 @@ uint32_t sha1coinhash(void *state, const void *input)
     SHA1((const unsigned char*)&str[i], 12, (unsigned char *)prehash);
 //#define TRIP
 #if defined(TRIP)
-    if ((prehash[0] & 0x0000ffff) == 0x000016b2) { // "sha"
+    if ((prehash[0] & 0x0000ffff) == searchchunk) {
       encodeb64wide((const unsigned char *)prehash, (unsigned short *)trip);
-      if (!memcmp(trip, "sha1", 4)) {
+      if (!memcmp(trip, searchtrip, 4)) {
         memcpy(tripkey, &str[i], 12);
         trip[12] = 0;
         //applog(LOG_INFO, "tripkey: #%s, trip: %s", tripkey, trip);
